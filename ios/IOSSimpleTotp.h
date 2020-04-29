709e2db034e31ea0ee15c322409bfea679d8b774
paul <pcapelo@libelulasoft.com>
2020-04-29 14:35:00 -0500

    ios version 1.1

---
+++ ios/IOSSimpleTotp.h
@@ -0,0 +1,17 @@
+//
+//  IOSNativeToast.h
+//  RNNativeToastLibrary
+//
+//  Created by Roi Berlin on 17/01/2019.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+//#import <Foundation/Foundation.h>
+//
+//NS_ASSUME_NONNULL_BEGIN
+//
+//@interface IOSSimpleTotp : NSObject
+//-(void) getTOTP:(NSString*) semilla, tiempo:(NSString*)tiempo, fecha:(NSString*) fecha
+//@end
+//
+//NS_ASSUME_NONNULL_END
