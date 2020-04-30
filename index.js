7590ff759df50de517184e0fea74c49193f61128
paul <pcapelo@libelulasoft.com>
2020-04-29 22:30:16 -0500

    ios version dev

--- index.js
+++ index.js
@@ -3,18 +3,25 @@
  * @email jhonnathanp27@gmail.com
  * @create date 2020-04-28 23:04:04
  */
-import { NativeModules,NativeEventEmitter } from 'react-native';
+// import { NativeModules,NativeEventEmitter } from 'react-native';
 
-const { RNTotplibe } = NativeModules;
+// const { RNTotplibe } = NativeModules;
 
 
-class TotpLibe extends NativeEventEmitter {
-  constructor() {
-		super(TotpLibe);
-  }
-  getTOTP(semilla,tiempo,fecha) {
-		return RNTotplibe.getTOTP(semilla,tiempo,fecha);
-	}
-}
+// class TotpLibe extends NativeEventEmitter {
+//   constructor() {
+// 		super(TotpLibe);
+//   }
+//   getTOTP(semilla,tiempo,fecha) {
+// 		return RNTotplibe.getTOTP(semilla,tiempo,fecha);
+// 	}
+// }
 
-export default new TotpLibe;
+// export default new TotpLibe;
+
+
+import { NativeModules } from 'react-native';
+
+const { RNNativeToastLibrary } = NativeModules;
+
+export default RNNativeToastLibrary;
