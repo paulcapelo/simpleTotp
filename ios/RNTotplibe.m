d523b52e9d3611158ccdc192394c8568166787cf
paul <pcapelo@libelulasoft.com>
2020-04-30 19:51:23 -0500

    ios version dev

--- ios/RNTotplibe.m
+++ ios/RNTotplibe.m
@@ -2,9 +2,21 @@
 #import <React/RCTBridgeModule.h>
 
 
-@interface RCT_EXTERN_MODULE(RNTotplibe, NSObject)
+@interface RCT_EXTERN_REMAP_MODULE(RNTotplibe,RNTotplibe, NSObject)
 
-RCT_EXTERN_METHOD(getTOTP:(NSString*)semilla tiempo:(NSString*)tiempo fecha:(NSString*)fecha (RCTResponseSenderBlock)callback)
+RCT_EXTERN_METHOD(getTOTP:(NSString*)semilla tiempo:(NSString*)tiempo fecha:(NSString*)fecha resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 
 
 @end
+
+
+//#import "RNTotplibe-Swift.h"
+////@interface RCT_EXTERN_REMAP_MODULE(RNTotplibe, RNTotplibe, NSObject)
+////
+////RCT_EXTERN_REMAP_METHOD(getTOTP:(NSString*)semilla tiempo:(NSString*)tiempo fecha:(NSString*)fecha
+////                    resolver:(RCTPromiseResolveBlock)resolve
+////                    rejecter:(RCTPromiseRejectBlock)reject)
+////
+//
+////RCT_EXTERN_METHOD(getTOTP:(NSString*)semilla tiempo:(NSString*)tiempo fecha:(NSString*)fecha (RCTResponseSenderBlock)callback)
+
