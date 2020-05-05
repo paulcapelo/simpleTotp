d1d31298d28331dadcd98aeb3820e98c1fb9b0d3
jhonnathan Paul <jhonnathanp27@gmail.com>
2020-05-04 22:15:23 -0500

    Update RNTotplibe.m

--- ios/RNTotplibe.m
+++ ios/RNTotplibe.m
@@ -1,5 +1,7 @@
-#import <React/RCTBridgeModule.h>
-#import <CommonCrypto/CommonHMAC.h>
+// #import <React/RCTBridgeModule.h>
+// #import <CommonCrypto/CommonHMAC.h>
+#import "React/RCTBridgeModule.h"
+#import "CommonCrypto/CommonHMAC.h"
 
 @interface RCT_EXTERN_MODULE(RNTotplibe, NSObject)
 // RCT_EXTERN_METHOD(getTOTP:(NSString *)semilla tiempo:(NSString *)tiempo fecha:(NSString *)fecha resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject );
