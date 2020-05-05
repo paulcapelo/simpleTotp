d64bfcab997fc136046030349a6263896cfcedc4
paul <pcapelo@libelulasoft.com>
2020-05-04 20:43:32 -0500

    comiint

--- ios/RNTotplibe.m
+++ ios/RNTotplibe.m
@@ -2,6 +2,6 @@
 #import <CommonCrypto/CommonHMAC.h>
 
 @interface RCT_EXTERN_MODULE(RNTotplibe, NSObject)
-RCT_EXTERN_METHOD(getTOTP:(NSString *)semilla tiempo:(NSString *)tiempo fecha:(NSString *)fecha resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject );
-// RCT_EXTERN_METHOD(getTOTP);
+// RCT_EXTERN_METHOD(getTOTP:(NSString *)semilla tiempo:(NSString *)tiempo fecha:(NSString *)fecha resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject );
+RCT_EXTERN_METHOD(getTOTP);
 @end
