4d6af34a0eb3a649fa7c9af14abf7ba606d45bb1
jhonnathan Paul <jhonnathanp27@gmail.com>
2020-05-04 22:16:11 -0500

    Update RNTotplibe-Bridging-Header.h

--- ios/RNTotplibe-Bridging-Header.h
+++ ios/RNTotplibe-Bridging-Header.h
@@ -2,6 +2,7 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-//#import "React/RCTBridgeModule.h"
-#import <React/RCTBridgeModule.h>
-#import <CommonCrypto/CommonHMAC.h>
+#import "React/RCTBridgeModule.h"
+#import "CommonCrypto/CommonHMAC.h"
+// #import <React/RCTBridgeModule.h>
+// #import <CommonCrypto/CommonHMAC.h>
